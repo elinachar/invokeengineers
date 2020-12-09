d1b379f4bf059d6c3a81b4fc733bf1cda2c9fc06
elinachar <elinachar@gmail.com>
2020-12-09 17:04:44 +0200

    Email configuration for production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Connect to  email provider in Plesk
+ActionMailer::Base.smtp_settings = {
+  address: 'localhost',
+	enable_starttls_auto: true,
+ 	port: 587,
+	authentication: :plain,
+  user_name: ENV['EMAIL_USERNAME'],
+  password: ENV['EMAIL_PASSWORD'],
+	openssl_verify_mode: 'none'
+}
